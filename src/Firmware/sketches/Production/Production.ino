48553e636df047bf0f127689818159b229d955ce
Bastian Raschke <bastian.raschke@posteo.de>
2016-07-31 22:57:11 +0200

    Experiments with DNS server

--- src/Firmware/sketches/Production/Production.ino
+++ src/Firmware/sketches/Production/Production.ino
@@ -1,4 +1,5 @@
 #include <ESP8266WiFi.h>
+#include <DNSServer.h>
 #include <ESP8266WebServer.h>
 #include <Adafruit_NeoPixel.h>
 
@@ -15,10 +16,6 @@
 #define WIFI_SSID                   "Bastis Pennyboard"
 #define WIFI_PASSWORD               "penny1337"
 
-#define WIFI_DEBUG_MODE_ENABLED     true
-#define WIFI_DEBUG_SSID             ""
-#define WIFI_DEBUG_PASSWORD         ""
-
 
 #define PIN_STATUSLED               LED_BUILTIN
 
@@ -27,12 +24,24 @@
 #define NEOPIXELS_BRIGHTNESS        255     // [0..255]
 
 Adafruit_NeoPixel neopixelStrip = Adafruit_NeoPixel(NEOPIXELS_COUNT, PIN_NEOPIXELS, NEO_GRB + NEO_KHZ800);
+
+DNSServer dnsServer = DNSServer();
 ESP8266WebServer webServer = ESP8266WebServer(80);
 
 /*
  * Neopixel effects
  *
  */
+
+void neopixel_off()
+{
+    for (uint16_t i = 0; i < neopixelStrip.numPixels(); i++)
+    {
+        neopixelStrip.setPixelColor(i, 0);
+    }
+
+    neopixelStrip.show();
+}
 
 void neopixel_showSingleColorWithDifferentWheelColorScene()
 {
@@ -61,6 +70,49 @@ void neopixel_showSingleColorWithDifferentWheelColorScene()
     neopixelStrip.show();
 }
 
+void neopixel_showGradientScene(const uint32_t color1, const uint32_t color2)
+{
+    const uint16_t neopixelCount = neopixelStrip.numPixels();
+
+    // Split first color to R, B, G parts
+    const uint8_t color1_r = (color1 >> 16) & 0xFF;
+    const uint8_t color1_g = (color1 >>  8) & 0xFF;
+    const uint8_t color1_b = (color1 >>  0) & 0xFF;
+
+    // Split second color to R, B, G parts
+    const uint8_t color2_r = (color2 >> 16) & 0xFF;
+    const uint8_t color2_g = (color2 >>  8) & 0xFF;
+    const uint8_t color2_b = (color2 >>  0) & 0xFF;
+
+    for(uint16_t i = 0; i < neopixelCount; i++)
+    {
+        float percentage = _mapPixelCountToPercentage(i, neopixelCount);
+
+        // Calculate the color of this iteration
+        // see: https://stackoverflow.com/questions/27532/ and https://stackoverflow.com/questions/22218140/
+        const uint8_t r = (color1_r * percentage) + (color2_r * (1 - percentage));
+        const uint8_t g = (color1_g * percentage) + (color2_g * (1 - percentage));
+        const uint8_t b = (color1_b * percentage) + (color2_b * (1 - percentage));
+
+        const uint32_t currentColor = neopixelStrip.Color(r, g, b);
+        neopixelStrip.setPixelColor(i, currentColor);
+    }
+
+    neopixelStrip.show();
+}
+
+float _mapPixelCountToPercentage(uint16_t i, float count)
+{
+    const float currentPixel = (float) i;
+    const float neopixelCount = (float) count;
+
+    const float min = 0.0f;
+    const float max = 1.0f;
+
+    return (currentPixel - 0.0f) * (max - min) / (neopixelCount - 0.0f) + min;
+}
+
+
 void blinkStatusLED(const int times)
 {
     for (int i = 0; i < times; i++)
@@ -88,55 +140,60 @@ void setupNeopixels()
     neopixelStrip.setBrightness(NEOPIXELS_BRIGHTNESS);
 }
 
+
+
+const uint16_t DNS_PORT = 53;
+IPAddress gatewayAddress = IPAddress(10, 0, 0, 1);
+IPAddress broadcastAddress = IPAddress(255, 255, 255, 0);
+
+
+
 void setupWifi()
 {
-    #if WIFI_DEBUG_MODE_ENABLED == true
+    WiFi.mode(WIFI_AP);
+    WiFi.softAPConfig(gatewayAddress, gatewayAddress, broadcastAddress);
+    WiFi.softAP(WIFI_SSID, WIFI_PASSWORD);
+}
 
-        Serial.printf("Connecting to %s\n", WIFI_SSID);
+void setupDNSServer()
+{
+    Serial.println("Starting DNS server...");
 
-        // Disable Wifi access point mode
-        WiFi.mode(WIFI_STA);
+    dnsServer.setTTL(300);
+    dnsServer.setErrorReplyCode(DNSReplyCode::NonExistentDomain);
 
-        WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-
-        while (WiFi.status() != WL_CONNECTED)
-        {
-            // Blink 2 times when connecting
-            blinkStatusLED(2);
-
-            delay(500);
-            Serial.print(".");
-        }
-
-        Serial.println();
-        Serial.println("WiFi connected.");
-
-        Serial.print("Obtained IP address: ");
-        Serial.println(WiFi.localIP());
-
-    #else
-
-        Serial.printf("Starting access point %s\n", WIFI_SSID);
-
-        WiFi.mode(WIFI_AP);
-        WiFi.softAP(WIFI_SSID, WIFI_PASSWORD);
-
-    #endif
+    dnsServer.start(DNS_PORT, "*", gatewayAddress);
 }
 
 void setupWebserver()
 {
+    Serial.println("Starting HTTP webserver...");
+
     webServer.begin();
 
     webServer.on( "/", handleRoot );
-    webServer.on( "/test.svg", drawGraph );
-    webServer.on( "/inline", []() {
-        webServer.send ( 200, "text/plain", "this works as well" );
-    } );
     webServer.onNotFound ( handleNotFound );
-    webServer.begin();
 
-    Serial.println ("HTTP webserver started.");
+    webServer.on( "/0", []() {
+        neopixel_off();
+        webServer.send(200, "text/plain", "OK");
+    });
+
+    webServer.on( "/1", []() {
+        neopixel_showSingleColorWithDifferentWheelColorScene();
+        webServer.send(200, "text/plain", "OK");
+    });
+
+    webServer.on( "/1", []() {
+
+        uint32_t color1 = 0xA92CCE;
+        uint32_t color2 = 0xCE2C2C;
+
+        neopixel_showGradientScene(color1, color2);
+        webServer.send(200, "text/plain", "OK");
+    });
+
+    webServer.begin();
 }
 
 void setup()
@@ -147,6 +204,8 @@ void setup()
     setupPins();
     setupNeopixels();
     setupWifi();
+
+    setupDNSServer();
     setupWebserver();
 
     neopixel_showSingleColorWithDifferentWheelColorScene();
@@ -154,31 +213,13 @@ void setup()
 
 void loop()
 {
+    dnsServer.processNextRequest();
     webServer.handleClient();
 }
 
 
 
 
-
-
-void drawGraph() {
-    String out = "";
-    char temp[100];
-    out += "<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" width=\"400\" height=\"150\">\n";
-    out += "<rect width=\"400\" height=\"150\" fill=\"rgb(250, 230, 210)\" stroke-width=\"1\" stroke=\"rgb(0, 0, 0)\" />\n";
-    out += "<g stroke=\"black\">\n";
-    int y = rand() % 130;
-    for (int x = 10; x < 390; x+= 10) {
-        int y2 = rand() % 130;
-        sprintf(temp, "<line x1=\"%d\" y1=\"%d\" x2=\"%d\" y2=\"%d\" stroke-width=\"1\" />\n", x, 140 - y, x + 10, 140 - y2);
-        out += temp;
-        y = y2;
-    }
-    out += "</g>\n</svg>\n";
-
-    webServer.send ( 200, "image/svg+xml", out);
-}
 
 void handleRoot() {
   webServer.send(200, "text/plain", "hello from esp8266!");
