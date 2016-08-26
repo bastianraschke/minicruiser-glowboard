347197296e9fcc2014523dae02e6956090c78f0d
Bastian Raschke <bastian.raschke@posteo.de>
2016-08-26 14:37:38 +0200

    First test with background image

---
+++ Design/Web-App/convertimage.py
@@ -0,0 +1,30 @@
+#!/usr/bin env python3
+
+output = ""
+
+count = 0
+
+f = open("./src/background.jpg", "rb")
+
+try:
+    while (True):
+
+        currentByte = f.read(1)
+
+        if (currentByte == ""):
+            break
+        else:
+
+            formattedChar = '0x' + format(ord(currentByte), '02X')
+
+            if ( count == 20 ):
+                output += formattedChar + ",\n"
+                count = 0
+            else:
+                output += formattedChar + ", "
+                count = count + 1
+
+finally:
+    f.close()
+
+print(output)
